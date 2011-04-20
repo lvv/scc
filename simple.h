23d6aab0f7987766ec3ec37c322f82691c69d849
Leonid Volnitsky <Leonid@Volnitsky.com>
2011-04-20 13:45:18 +0300

    -- fixed: str errors

--- simple.h
+++ simple.h
@@ -263,11 +263,11 @@ struct str: string {
 		 return I;
 	}
 
+	str  operator +  (const char* s) { return  *(string*)this +  string(s); }	// converter to std::string&
+	//str& operator += (const char* s) { return  *this += string(s); }	// converter to std::string&
+
 	// prefix/postfix inc/dec
-	//str operator % (string s) 	{ return  (*(string*)this + (string)s); }	// converter to std::string&
-	//str operator % (const char* s) { return  (*(string*)this + string(s)); }	// converter to std::string&
-	str operator + (const char* s) { return  (*(string*)this + string(s)); }	// converter to std::string&
-	str& operator += (int I) { ostringstream OS;   OS << *this + I;   this->string::assign(OS.str());  return *this; }
+
 	int operator++() {                   return *this = *this + 1; }
 	int operator--() {                   return *this = *this - 1; }
 	int operator++(int) { int old = int(*this); *this = *this + 1; return old; }
