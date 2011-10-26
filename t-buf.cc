21d164fef7351f07f54e76f0e4a42bc5b9d1d91b
Leonid Volnitsky <Leonid@Volnitsky.com>
2011-10-26 18:29:20 +0300

    t-buf.cc works without check for end-of-buf

--- t-buf.cc
+++ t-buf.cc
@@ -1,42 +1,78 @@
 #include <errno.h>
 #include <unistd.h>
-#include <simple.h>
-#include <scc.h>
+#include <scc/simple.h>
+#include <scc/scc.h>
 
-	const size_t buf_size=1000;
-	char* buf;
 
-int readline(int fd, char *buf, int nbytes) {
-	int     numread = 0;
-	int     returnval;
+struct buf_t {
+			char		*bob, *eob;	// buffer dimentions
+			char		*bod, *eod;	// unused data in buffer
+	const static	size_t		buf_size=1000;
+			int		fd;		// file
+			bool		good_file;	// !eof
 
-	while (numread < nbytes - 1) {
-		returnval = read(fd, buf + numread, 1);
-
-		if ((returnval == -1) && (errno == EINTR))	continue;
-		if ((returnval == 0) && (numread == 0))		return 0;
-		if ( returnval == 0)				break;
-		if ( returnval == -1)				return -1;
-
-		numread++;
-		
-		if (buf[numread - 1] == '\n') {
-			buf[numread] = '\0';
-			return numread;
-		}
+	explicit	buf_t 		(int fd) : fd(fd), good_file(true) {
+		bob = bod = eod =  new char[buf_size];  
+		eob = bob + buf_size;
 	}
-	errno = EINVAL;
-	return -1;
-}
 
-bool get_rec(int fd, strr) {
-	return false;
-}
+
+	size_t		capacity	()	{ return buf_size; }
+	size_t		size		()	{ return eod-bod; }
+
+
+	bool		fill		()	{
+		size_t buf_free_space = eob-eod;
+		ssize_t got;
+		if (buf_free_space > 0) { 
+			retry:
+			got = read (fd, eod,  buf_free_space);
+			if (got == -1  &&  errno == EINTR)	goto  retry;
+			if (got <=  0)				return  false;
+
+			eod += got;
+		}
+		return  true;  				// TODO
+	}
+
+
+	bool		get_rec		(strr& sr)	{
+
+		if (!good_file)   return false; 
+
+		char *bor, *eor; // record
+		bor = eor = bod;
+
+		do {
+			size_t	unchecked = eod - eor;
+			
+			if ( unchecked == 0 )  {
+				if ( !(good_file = fill()) )  {
+					if ( bor == eor ) 	return false;
+					else 			goto return_strr;
+				}
+			}
+								assert((unchecked = eod - eor,  unchecked > 0));
+		} while ( *eor++ != '\n');
+
+
+		return_strr:
+			sr.B = bor;
+			sr.E = eor-1;
+			bod = eor;
+			return true;
+	}
+
+};
+
 
 int main() {
-	buf = new char[buf_size];
+	buf_t buf(0);
 	strr sr;
-	while(get_rec(0, sr))
-		__ sr;
+	int i=0;
+	while(buf.get_rec(sr))
+		__ i++ ^  sr;
+	__ "total: ", i;
+
 }
 
