7044d0d2be40a962177d347b56f68257c86fce4f
Leonid Volnitsky <Leonid@Volnitsky.com>
2011-12-25 13:44:58 +0200

    t-meta.cc

---
+++ t-meta.cc
@@ -0,0 +1,105 @@
+//#include<iostream>
+//#include<type_traits>
+//#include"lvv/lvv.h"
+#include<scc/simple>
+
+using namespace std;
+
+	template<class T> T& lvalue_of_type();
+	template<class T> T  rvalue_of_type();
+
+	template<class T>
+struct is_printable {
+	template<class U>  static char  test (char(*)[sizeof( lvalue_of_type<std::ostream>() << rvalue_of_type<U>())]);
+	template<class U>  static long  test (...);
+
+	enum { value = 1 == sizeof test<T>(0) };
+	typedef std::integral_constant<bool, value> type;
+};
+
+
+
+	template<typename T>
+struct has_const_iterator {
+	template<typename C>  static char  test (typename C::const_iterator*);
+	template<typename C>  static long  test (...);
+	static const bool value = sizeof(test<T>(0)) == 1;
+	typedef T type;
+};
+
+
+
+	template <typename T>
+struct is_container {	// from http://stackoverflow.com/questions/4347921/sfinae-compiler-troubles
+	template <typename U>
+	static char test(
+		U* u,
+		typename U::const_iterator b = ((U*)0)->begin(),
+		typename U::const_iterator e = ((U*)0)->end()
+	);
+	template <typename U> static long test(...);
+
+	enum { value = sizeof test<T>(0) == 1 };
+};
+
+struct out_t {};
+
+template<typename S> struct is_sink{ const static bool value =  std::is_same<S,std::ostream>::value || std::is_same<S,out_t>::value; };
+
+int main() {
+
+	cout << left;  cout.width(25);
+	__ endl, "type",  "has_CI\t", "is_C", "is_P", endl; 
+
+	#define for_T(name) cout.width(25);  _ name; \
+		outln ("\t") \
+		<<  is_container<T>::value\
+		<<  has_const_iterator<T>::value\
+		<<  is_printable<T>::value\
+	;
+
+	{ typedef vector<int>  T;
+	for_T(   "vector<int>"); }
+
+	{ typedef set<int>  T;
+	for_T(   "set<int>"); }
+
+	{ typedef set<int,std::greater<int>>  T;
+	for_T(   "set<int,std::greater<int>>"); }
+
+	{ typedef map<string, int>  T;
+	for_T(   "map<string, int>"); }
+
+	{ typedef pair<string,int>  T;
+	for_T(   "pair<string,int>"); }
+
+	{ typedef tuple<string,int>  T;
+	for_T(   "tuple<string,int>"); }
+
+	{ typedef string  T;
+	for_T(   "string"); }
+
+	{ typedef int	T[5];
+	for_T(   "int[5]"); }
+
+	{ typedef char	T[5];
+	for_T(   "char[5]"); }
+
+	{ typedef const char*  T;
+	for_T(   "const char*");  }
+
+	{ typedef int*  T;
+	for_T(   "int*");  }
+
+	{ struct T {int a;}; 
+	for_T(   "struct");  }
+
+	cout
+		<< is_sink<int>::value
+		<< is_sink<ostream>::value
+		<< is_sink<out_t>::value
+		;
+}
+
+
+	
