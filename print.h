5e457802e0e97250add5d874f26bbe6510745bee
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-01-05 12:27:10 +0200

    op,(ostream,T)  now uses space as seperator,   op^ was removed

--- print.h
+++ print.h
@@ -28,11 +28,9 @@ struct  out {
 	void send_sep() { if (sep && !first_use) cout << sep;  first_use = false; };
 
 	template<typename T>	out&  operator<<  (T x)	{ send_sep();  cout <<         x;	return *this; };
-	template<typename T>	out&  operator,   (T x)	{ send_sep();  cout <<         x;	return *this; };
+	template<typename T>	out&  operator,   (T x)	{ send_sep();  cout << " " << x;	return *this; };
 	template<typename T, size_t N>	out&  operator<<  (const T (&x)[N])	{ send_sep();  cout <<        x;	return *this; };
 	template<typename T, size_t N>	out&  operator,   (const T (&x)[N])	{ send_sep();  cout <<        x;	return *this; };
-	//template<typename T>	out&  operator|   (T x)	{ send_sep();  cout << " "  << x;	return *this; };
-	template<typename T>	out&  operator^   (T x)	{ send_sep();  cout << " " << x;	return *this; };
 
 	// endl, hex, ..
 				out&  operator<< (std::ostream&  (*pf) (std::ostream& ) ) { cout << pf;  return *this; };
