03a3994c1f8089acbb6ab11d2e6ee404dff78e6c
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-01-14 20:19:27 +0200

    sccpp
    g++ -Wall -ggdb -D_GLIBCXX_DEBUG      sccpp.cc   -o sccpp
    -------------------------------------   match
    
    -------------------------------------   match
    aa;
        aa;
            aa;
                aa
    -------------------------------------   match
    bb
        bb   *** id ***
            bb
    -------------------------------------   match
    aa;bb;
        aa;bb;
            aa;
                aa
            bb;
                bb
    -------------------------------------   match
    aa;bb
        aa;
            aa;
                aa
        bb   *** id ***
            bb
    -------------------------------------   match
    aa;bb;cc
        aa;bb;
            aa;
                aa
            bb;
                bb
        cc   *** id ***
            cc
    -------------------------------------   match
    aa;{bb;}cc
        aa;{bb;}
            aa;
                aa
            {bb;}
                bb;
                    bb;
                        bb
        cc   *** id ***
            cc

--- sccpp.cc
+++ sccpp.cc
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 #include <boost/xpressive/xpressive.hpp>
 
 using namespace boost::xpressive;
@@ -8,8 +9,9 @@ using namespace std;
 // Displays nested results to std::cout with indenting
 struct output_nested_results {
 	int tabs_;
+	regex_id_type id_;
 
-	output_nested_results( int tabs = 0 ) : tabs_( tabs ) {}
+	output_nested_results( int tabs, regex_id_type id) : tabs_( tabs ), id_(id) {}
 
 	        template<typename BidiIterT>
 		void
@@ -20,32 +22,48 @@ struct output_nested_results {
 		std::fill_n( std::ostream_iterator<char_type>( std::cout ), tabs_ * 4, space_ch );
 
 		// output the match
-		std::cout << what[0] << '\n';
+		std::cout << what[0];
+		if (what.regex_id() == id_) std::cout << "   *** id ***";
+		std::cout <<  '\n';
 
 		// output any nested matches
 		std::for_each (
 			what.nested_results().begin(),
 			what.nested_results().end(),
-			output_nested_results( tabs_ + 1 )
+			output_nested_results( tabs_ + 1, id_)
 		);
 	}
 };
 
 int main() {
-	string s( "9*(10+3)-1/2" );
+
+	vector<string> S;
+		S.push_back("");
+		S.push_back("aa;");
+		S.push_back("bb");
+		S.push_back("aa;bb;");
+		S.push_back("aa;bb");
+		S.push_back("aa;bb;cc");
+		S.push_back("aa;{bb;}cc");
 
 	smatch	what;
-	sregex	group, factor, term, expression;
+	string fmt("$1 << $2 >> ;");
+	sregex	snippet, expr, last_expr, code, block, comment, statment;
 
-	group       = '(' >> by_ref(expression) >> ')';
-	factor      = +_d | group;
-	term        = factor >> *(('*' >> factor) | ('/' >> factor));
-	expression  = term >> *(('+' >> term) | ('-' >> term));
+		comment		= "//" >> *~_ln >> (_ln | eos);
+		expr		= +_w;
+		last_expr	= expr >> eos;
+		statment	= expr >> ';';
+		code		= +( statment | by_ref(block) );
+		block		= '{' >> by_ref(code) >> '}';
+		snippet		= (s1 = !code) >> (s2 = last_expr);
 
+	for (unsigned i=0;  i<S.size();  i++) {
+		cout	<< "-------------------------------------   "
+			<< (regex_match(S[i], what, snippet) ? "match" : "NO-match") << "\n";
+		output_nested_results(0,last_expr.regex_id())(what);
+		cout	<< S[i] << "  -->  "
+			<<  regex_replace(S[i], snippet, fmt) << '\n';
 
-	sregex	snippet;
-
-	if (regex_match(s, what, expression)) {
-		output_nested_results()(what);
 	}
 }
