910258fad5afe5ab9b98a8d2e27c92fa72e8232d
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-01-16 22:27:20 +0200

    -- expr grammar

--- sccpp.h
+++ sccpp.h
@@ -1,19 +1,20 @@
 
 	// full C++ grammar:  http://www.nongnu.org/hcb/#escape-sequence
 
-	sregex	ws, snippet, expr, last_expr, code, block, statement, compaund_expr, op, word, paran, str, ch, esc,
-		comment_cpp, comment_c, comment,
-		valid_snippet, with_last_snippet;
+	sregex	atom, expr1, expr, code1, code, block, statement, compaund_expr,
+		op, postfix_op, prefix_op,
+		word, paran, str, ch, esc,
+		comment_cpp, comment_c, comment, blnk,
+		valid_snippet, with_last;
 
-	ws		= *_s;
-	comment_cpp	= "//" >> *~_ln >> (_ln | eos);
-	comment_c	= "/*" >> *_ >> "*/";
+	// BLANK
+	comment_cpp	= as_xpr('/') >> '/' >> *~_ln >> (_ln | eos);
+	comment_c	= as_xpr('/') >> '*' >>  -*_ >>  '*' >> '/';
 	comment		= comment_c | comment_cpp;
-	op		= (set =  '$', '+', '-', '*', '/', '%', '&', '^', '|', '?', ':', '=', ',', '.', '<', '>') |
-		"::" | "++" | "--" | "->" | ".*" | "->*" | ">>" | "<<" | "&&" | "||" | "<=" | ">=" | "!=" |
-		(( as_xpr(">>") | "<<" | (set = '<', '>', '=', '!', '+', '-', '*', '/', '%', '&', '^', '|')) >> '=');
+	blnk		= *_s;// >> *(comment >> *_s);
 
-	word		=  _b >> +_w >> _b;
+
+	///  STRING
 	esc		= as_xpr('\\') >>
 				(
 					(set= '\"',  '0', 'a', 'b', 'f', 'n', 'r', 't', 'v')
@@ -24,17 +25,51 @@
 				);
 
 
-	str		= as_xpr('\"') >> *(~(set='\"','\\') |  esc) >> '\"';
-	ch		= as_xpr('\'') >> +(~as_xpr('\'') |  "\\\'") >> '\'';
-	expr		= ws >> +((word|comment|op|str|ch|by_ref(paran)|by_ref(compaund_expr)) >> ws);
+	str		= as_xpr('\"') >> -*(~(set='\"','\\') /*|  esc*/) >> '\"';
+	ch		= as_xpr('\'') >> -+~as_xpr('\'') >> '\'';
 
-	paran		= ('(' >> !by_ref(expr) >> ')') |
-			  ('[' >> !by_ref(expr) >> ']') |
-			  ('{' >> !by_ref(expr) >> '}');
-	last_expr	= expr >> eos;
-	statement	= expr >> ';' >> ws;
-	compaund_expr	= as_xpr("({") >>  by_ref(code) >> "})";
-	code		= +( statement | by_ref(block) );
-	block		= ws >> '{' >> by_ref(code) >> '}' >> ws;
-	valid_snippet		= bos >> (!(s1 = code)|ws) >> !(s2 = last_expr) >> eos;
-	with_last_snippet	= bos >> (!(s1 = code)|ws) >>  (s2 = last_expr) >> eos;
+	////  OP
+	// & $ ? && &
+	op		= (set= '+','-', '*', '/', '%',  '|',  ':', '=', ',', '.', '<', '>')
+				| (as_xpr('=')>> '=')
+				//| as_xpr("::") |  "->" | ".*" | "->*" | ">>" | "<<" |  "||" | "<=" | ">=" | "!="
+				//| (( as_xpr(">>") | "<<" | (set = '<', '>', '=', '!', '+', '-', '*', '/', '%',  '^', '|')) >> '=')
+				;
+
+	//prefix_op	= sregex::compile("[-+^!]|(\+\+)|(--)");
+	prefix_op	= (set =  '+', '-', '*', '^', '!', '~');// | as_xpr("++") | as_xpr("--");
+
+	//postfix_op	= sregex::compile("(++)|(--)");
+	postfix_op	= (as_xpr('+') >> '+') | (as_xpr('-') >> '-');
+
+	/////  EXPR
+	word		=   +_w ;
+	//word		= ~after(set[_w|_d|'$']) >> +(_w | _d | '$') >> ~before(set[_w|_d|'$']);
+	atom		= word ; //| ch | str | by_ref(paran) | by_ref(compaund_expr);
+
+
+	expr1		= (*(prefix_op >> blnk) >>   by_ref(atom)  >> *(blnk >> postfix_op));
+	expr		=  expr1 >> *(blnk >> op >> blnk  >> expr1);
+				;
+	//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	//expr1		=  word|op|str|ch|by_ref(paran)|by_ref(compaund_expr);
+	//expr		=  expr1 >> *(blnk >> expr1);
+
+
+	paran		= ('(' >> blnk >> !(by_ref(expr) >> blnk) >> ')') |
+			  ('[' >> blnk >> !(by_ref(expr) >> blnk) >> ']') |
+			  ('<' >> blnk >> !(by_ref(expr) >> blnk) >> '>');
+			  //('{' >> blnk >> !(by_ref(expr) >> blnk) >> '}');
+
+	statement	= by_ref(expr) >> blnk >> ';';
+
+	code1		= statement | by_ref(block);
+	code		= code1 >> *(blnk >> code1);
+	compaund_expr	= as_xpr('(') >> '{' >>  blnk >> by_ref(code) >> blnk >> '}' >> ')';
+
+	block		= '{' >> blnk >> by_ref(code) >> blnk >> '}';
+
+	valid_snippet	= bos >> !(s1 = (blnk >> code >> blnk)) >> !(s2 = expr) >> (s3 = blnk) >> eos;
+	with_last	= bos >> !(s1 = (blnk >> code >> blnk)) >>  (s2 = expr) >> (s3 = blnk) >> eos;
--- u-sccpp.cc
+++ u-sccpp.cc
@@ -40,74 +40,139 @@ struct output_nested_results {
 		);
 	}
 };
+	#define LVV_STR(x) __LVV_STR__(x)
+	#define __LVV_STR__(x) #x
+	#define		Q(r)		r,  LVV_STR(r)
 
 struct  test {
-	test(bool valid,  string s) : valid(valid), s(s) {};
+	test(bool valid, sregex &r, string rname,  string s) : valid(valid), s(s), r(r), rname(rname) {};
 	bool	valid;
 	string	s;
+	sregex	r;
+	string	rname;
 };
 
 
 int main() {
 
-	std::vector<test> C;
-		C.push_back(test(1,	""					));
-		C.push_back(test(1,	" "					));
-		C.push_back(test(1,	"aa;"					));
-		C.push_back(test(1,	"bb"					));
-		C.push_back(test(1,	"aa;bb;"				));
-		C.push_back(test(1,	"aa;bb"					));
-		C.push_back(test(1,	"aa;bb;cc"				));
-		C.push_back(test(1,	"aa;{ bb+ +  ; } - cc  "		));
-		C.push_back(test(1,	"aa;{bb+;} - cc  "			));
-		C.push_back(test(0,	"(()"					));
-		C.push_back(test(0,	"([)]"					));
-		C.push_back(test(0,	"(;)"					));
-		C.push_back(test(1,	"aa[[(1+2)[]]()]"			));
-
-		C.push_back(test(0,	"\""					));
-		C.push_back(test(0,	"\"\\\""				));
-		C.push_back(test(1,	"\"\""					));
-		C.push_back(test(1,	"\"\\\"\""				));
-		C.push_back(test(0,	"''"					));
-		C.push_back(test(1,	"';'"					));
-		C.push_back(test(1,	"'\"'"					));
-		C.push_back(test(1,	"\";})'{(\""				));
-
-		C.push_back(test(1,	"last; /* ;)[\" */"			));
-		C.push_back(test(1,	"last; // ;)[\" "			));
-		C.push_back(test(1,	"ab; // ;)[\"\n last "			));
-
-		C.push_back(test(1,	"ab; ({last1;last2;}) "			));
-
-		C.push_back(test(1,	"int A[2] = {1,2}; A"			));
-		C.push_back(test(1,	"$"					));
-		C.push_back(test(1,	"$1=9;"					));
-		C.push_back(test(1,	"WRL n+=$3;  n"				));
-		C.push_back(test(1,	"vint A{1,2,3}; copy(A.bb, A.ee, oi); str S(\"ABC\"); copy(S.bb, S.ee, oi); cout<<endl;"));
-		C.push_back(test(1,	"vint A{1,2,3};"			));
-		C.push_back(test(1,	"copy(A.bb, A.ee, oi);"			));
-		C.push_back(test(1,	"copy(A.bb, A.ee, oi)z;"		));
-		C.push_back(test(1,	"copy(A.bb, A.ee, oi)"			));
-		C.push_back(test(1,	"copy()"				));
-		C.push_back(test(1,	"a.b"					));
-		C.push_back(test(1,	"{}"					));
-		C.push_back(test(1,	"{ fld f; str s(\"abc\"); f.assign(s.bb, s.ee); 	__ f;  }"));
-		C.push_back(test(1,	"\n\t{ fld f; str s(\"abc\"); f.assign(s.bb, s.ee);\t__ f;  }\n"));
-		C.push_back(test(1,	"// not-last"				));
-
-	smatch	what;
-	string fmt("$1 << $2 >> ;");
 	#include "scc/sccpp.h"
 
+	std::vector<test> C;
+		C.push_back(test(1, Q(valid_snippet),	""					));
+		C.push_back(test(1, Q(valid_snippet),	" "					));
+		C.push_back(test(1, Q(valid_snippet),	"aa;"					));
+		C.push_back(test(1, Q(valid_snippet),	"bb"					));
+		C.push_back(test(1, Q(valid_snippet),	"aa;bb;"				));
+		C.push_back(test(1, Q(valid_snippet),	"aa;bb"					));
+		C.push_back(test(1, Q(valid_snippet),	"aa;bb;cc"				));
+		C.push_back(test(1, Q(valid_snippet),	"    aa;{++bb;} - LAST"			));
+		C.push_back(test(1, Q(valid_snippet),	"int aa;{++bb;} - LAST"			));
+		C.push_back(test(1, Q(valid_snippet),	"int/aa;{++bb;} - LAST"			));
+		C.push_back(test(1, Q(valid_snippet),	" aa; bb; LAST"			));
+		C.push_back(test(1, Q(valid_snippet),	"aa; bb; LAST"			));
+		C.push_back(test(0, Q(valid_snippet),	"(()"					));
+		C.push_back(test(0, Q(valid_snippet),	"([)]"					));
+		C.push_back(test(0, Q(valid_snippet),	"(;)"					));
+		C.push_back(test(1, Q(expr),	"1"					));
+		C.push_back(test(0, Q(expr),	"1-"					));
+		C.push_back(test(1, Q(expr),	"-a"					));
+		C.push_back(test(1, Q(expr),	"-*$"					));
+		C.push_back(test(0, Q(expr),	"==a"					));
+		C.push_back(test(0, Q(expr),	"=="					));
+		C.push_back(test(0, Q(expr),	"="					));
+		C.push_back(test(1, Q(expr),	"b==a"					));
+		C.push_back(test(0, Q(expr),	"b=="					));
+		C.push_back(test(0, Q(expr),	"=a"					));
+		C.push_back(test(0, Q(expr),	"=/a"					));
+		C.push_back(test(0, Q(expr),	"=a/"					));
+
+		C.push_back(test(0, Q(expr),	"/*0x1"					));
+		C.push_back(test(0, Q(expr),		"/*aa"					));
+		C.push_back(test(0, Q(expr),		" /*aa"					));
+		C.push_back(test(0, Q(expr),		"/aa"					));
+		C.push_back(test(1, Q(comment),		"//"					));
+		C.push_back(test(1, Q(comment),		"/**/"					));
+		C.push_back(test(0, Q(comment),		"/*/"					));
+		C.push_back(test(0, Q(comment),		"/*"					));
+		C.push_back(test(0, Q(expr),		"/"					));
+		C.push_back(test(0, Q(expr),		" / "					));
+		C.push_back(test(0, Q(comment),		"/*aa"					));
+		C.push_back(test(0, Q(expr),		"/*aa"					));
+		C.push_back(test(0, Q(expr),		"/*a"					));
+		C.push_back(test(0, Q(expr),		"/aa"					));
+		C.push_back(test(0, Q(expr),		"/+aa"					));
+		C.push_back(test(0, Q(expr),		"(/aa)"					));
+		C.push_back(test(0, Q(expr),		"(/+aa)"				));
+		C.push_back(test(0, Q(expr),		"%aa"					));
+		C.push_back(test(0, Q(expr),		"/*AA"					));
+		C.push_back(test(0, Q(expr),		"/*AAAA"				));
+		C.push_back(test(1, Q(expr),		"bbb/*AAAA"				));
+		C.push_back(test(0, Q(expr),		"bbb/*"				));
+		C.push_back(test(0, Q(expr),		"/ *aa"				));
+		C.push_back(test(0, Q(expr),		" / *aa"				));
+		C.push_back(test(1, Q(expr),		"bbb%/*AAAA"				));
+		C.push_back(test(0, Q(expr),		"%*aa"					));
+		C.push_back(test(1, Q(expr),		"*aa"					));
+		C.push_back(test(0, Q(expr),		"/*aa"					));
+		C.push_back(test(0, Q(expr),		"/1"					));
+		C.push_back(test(0, Q(expr),		"+/1"					));
+		C.push_back(test(0, Q(expr),		"/*aa"					));
+		C.push_back(test(0, Q(expr),		"/*aa"					));
+		C.push_back(test(0, Q(expr),		"/aa"					));
+		C.push_back(test(0, Q(expr),		"aa*/"					));
+
+		C.push_back(test(1, Q(valid_snippet),	"/*0x1*/"				));
+
+		C.push_back(test(1, Q(valid_snippet),	"//"					));
+		C.push_back(test(1, Q(valid_snippet),	"aa[[(1+2)[]]()]"			));
+		C.push_back(test(1, Q(expr),		"aa[[(1+2)[]]()]"			));
+		C.push_back(test(1, Q(paran),		"[[(1+2)[]]()]"				));
+		C.push_back(test(0, Q(str),		"\""					));
+		C.push_back(test(0, Q(str),		"\"\\\""				));
+		C.push_back(test(1, Q(str),		"\"\""					));
+		C.push_back(test(1, Q(valid_snippet),	"\"\\\"\""				));
+		C.push_back(test(0, Q(valid_snippet),	"''"					));
+		C.push_back(test(1, Q(valid_snippet),	"';'"					));
+		C.push_back(test(1, Q(valid_snippet),	"'\"'"					));
+		C.push_back(test(1, Q(valid_snippet),	"\";})'{(\""				));
+		C.push_back(test(1, Q(valid_snippet),	"last; /* ;)[\" */"			));
+		C.push_back(test(1, Q(valid_snippet),	"last; /* ;)[\" */ "			));
+		C.push_back(test(1, Q(valid_snippet),	"last; /*  */ "				));
+		C.push_back(test(1, Q(valid_snippet),	"last; // ;)[\" "			));
+		C.push_back(test(1, Q(valid_snippet),	"ab; // ;)[\"\n last "			));
+		C.push_back(test(1, Q(valid_snippet),	"ab; ({last1;last2;}) "			));
+		C.push_back(test(1, Q(valid_snippet),	"int A[2] = {1,2}; A"			));
+		C.push_back(test(1, Q(valid_snippet),	"$"					));
+		C.push_back(test(1, Q(valid_snippet),	"$1=9;"					));
+		C.push_back(test(1, Q(valid_snippet),	"WRL n+=$3;  n"				));
+		C.push_back(test(1, Q(valid_snippet),	"vint A{1,2,3}; copy(A.bb, A.ee, oi); str S(\"ABC\"); copy(S.bb, S.ee, oi); cout<<endl;"));
+		C.push_back(test(1, Q(valid_snippet),	"vint A{1,2,3};"			));
+		C.push_back(test(1, Q(valid_snippet),	"copy(A.bb, A.ee, oi);"			));
+		C.push_back(test(1, Q(valid_snippet),	"copy(A.bb, A.ee, oi)z;"		));
+		C.push_back(test(1, Q(valid_snippet),	"copy(A.bb, A.ee, oi)"			));
+		C.push_back(test(1, Q(valid_snippet),	"copy()"				));
+		C.push_back(test(1, Q(valid_snippet),	"a.b"					));
+		C.push_back(test(1, Q(valid_snippet),	"{}"					));
+		C.push_back(test(1, Q(valid_snippet),	"// not-last"				));
+		C.push_back(test(1, Q(valid_snippet),	"// not-last\n"				));
+		C.push_back(test(1, Q(valid_snippet),	"last; // not-last"			));
+		C.push_back(test(1, Q(valid_snippet),	"last; // not-last\n"			));
+		C.push_back(test(1, Q(valid_snippet),	"first; // not-last\n last"		));
+		C.push_back(test(1, Q(valid_snippet),	"first; // not-last\n non-last /*comm*/"));
+		C.push_back(test(1, Q(valid_snippet),	"/*com*/"));
+
+	smatch	what;
+
+	//string fmt("$1 <<== $2 ==>> ");
+	string fmt("\n\t&: $&\n\t1: $1 \n\t2: $2");
 
 	for (unsigned i=0;  i<C.size();  i++) {
 		//output_nested_results(0,last_expr.regex_id())(what);
-		bool match = regex_match(C[i].s, what, valid_snippet);
-		cout	<< (C[i].valid == match ? "OK     " : "ERROR  ") << (match ? "  valid" : "invalid") << "   ";
-		cout	<< ">" << C[i].s << "<";
+		bool match = regex_match(C[i].s, what, C[i].r);
+		cout	<< '\n' << (C[i].valid == match ? "OK    " : "ERROR ") << (match ? "  valid" : "invalid") << "   ";
+		cout	<< "\n\tN: " << C[i].rname ;
+		cout	<< "\n\tS: " << C[i].s ;
 
-		if (match) cout << "   --->   " <<  ">" << regex_replace(C[i].s, with_last_snippet, fmt) << "<";
-		cout << '\n';
+		if (match) cout << "\n\tR: " <<  regex_replace(C[i].s, with_last, fmt) << '\n';
 	}
 }
