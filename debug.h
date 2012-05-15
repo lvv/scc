b58812b48e5ed3ae45fe4508e49960bc25cfc189
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-05-15 08:08:06 +0300

    debug.h,  type2name

---
+++ debug.h
@@ -0,0 +1,7 @@
+#include <cxxabi.h>
+	template <typename T>
+const char* type2name() {
+	const char* t_name = typeid(T).name();
+	int status;
+	return abi::__cxa_demangle(t_name, NULL, NULL, &status);
+}
