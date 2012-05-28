42dda8f7bb5d92029ba55f80a1b7be38b12cb941
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-05-28 13:28:04 +0300

    --

--- stl.h
+++ stl.h
@@ -172,9 +172,9 @@ operator--      (Ct&& C, int)    { C.pop_back();    return  std::forward<Ct>(C);
 
 		////// MUL 
 
-			template<typename U>              static eIF< has_resize<U>::value,void>  ct_resizer(U& D,      size_t n)  { D.resize(n);};
-			template<typename U>              static eIF<!has_resize<U>::value,void>  ct_resizer(U& D,      size_t n)  {};
-			template<typename U,  size_t N>   static void                             ct_resizer(U (&D)[N], size_t n)  {}; 
+			template<typename U>              static eIF< has_resize<U>::value>  ct_resizer(U& D,      size_t n)  { D.resize(n);};
+			template<typename U>              static eIF<!has_resize<U>::value>  ct_resizer(U& D,      size_t n)  {};
+			template<typename U,  size_t N>   static void                        ct_resizer(U (&D)[N], size_t n)  {}; 
 
 
 		// Ct * f
