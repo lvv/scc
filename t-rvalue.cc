ffe5813b8dbde71f8d2e4b212b26434099c08efb
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-06-16 08:51:26 +0300

    forward_cl_elem
    ----------------------------------------------------------  FORWARD_CL_ELEM
    forward_cl_elem<const vint>       	C&&
    forward_cl_elem<volatile vint>       	&&
    forward_cl_elem<const volatile vint>       	C&&
       forward_cl_elem<vint>&&       	&&
    forward_cl_elem<const vint>&&       	C&&
        forward_cl_elem<vint>&       	&
    forward_cl_elem<const vint>&       	C&

--- t-rvalue.cc
+++ t-rvalue.cc
@@ -11,7 +11,8 @@ is_pointer -- http://stackoverflow.com/questions/3177686/how-to-implement-is-poi
 using namespace std;
 
 
-
+template<typename Cl>
+using  forward_cl_elem  =  typename  copy_rcv<Cl&&, cl_value_type<Cl>>::type;
 
 
 int main() {
@@ -28,9 +29,10 @@ int main() {
          #define LVV_STR(x) __LVV_STR__(x)
          #define __LVV_STR__(x) #x
 
+
+
 cout << "----------------------------------------------------------  NOP\n";
 #define DO_NOP(type)	 { typedef type  T;  for_T(LVV_STR(type));  }
-
 	DO_NOP(const int)
 	DO_NOP(volatile int)
 	DO_NOP(const volatile int)
@@ -38,6 +40,36 @@ cout << "----------------------------------------------------------  NOP\n";
 	DO_NOP(const int&&)
 	DO_NOP(int&)
 	DO_NOP(const int&)
+
+cout << "----------------------------------------------------------  NOP - VECTOR VALUE TYPE\n";
+#define DO_NOP_V_VT(type)	 { typedef type  T;  for_T(LVV_STR(type));  }
+	DO_NOP_V_VT(const vint::value_type)
+	DO_NOP_V_VT(volatile vint::value_type)
+	DO_NOP_V_VT(const volatile vint::value_type)
+	DO_NOP_V_VT(vint::value_type&&)
+	DO_NOP_V_VT(const vint::value_type&&)
+	DO_NOP_V_VT(vint::value_type&)
+	DO_NOP_V_VT(const vint::value_type&)
+
+cout << "----------------------------------------------------------  NOP - VECTOR CL_TRAITS<>::VALUE TYPE\n";
+#define DO_NOP_V_VT(type)	 { typedef type  T;  for_T(LVV_STR(type));  }
+	DO_NOP_V_VT(cl_value_type<const vint>)
+	DO_NOP_V_VT(cl_value_type<volatile vint>)
+	DO_NOP_V_VT(cl_value_type<const volatile vint>)
+	DO_NOP_V_VT(cl_value_type<vint>&&)
+	DO_NOP_V_VT(cl_value_type<const vint>&&)
+	DO_NOP_V_VT(cl_value_type<vint>&)
+	DO_NOP_V_VT(cl_value_type<const vint>&)
+
+cout << "----------------------------------------------------------  FORWARD_CL_ELEM\n";
+#define DO_FWD_CL_ELEM(type)	 { typedef type  T;  for_T(LVV_STR(type));  }
+	DO_FWD_CL_ELEM(forward_cl_elem<const vint>)
+	DO_FWD_CL_ELEM(forward_cl_elem<volatile vint>)
+	DO_FWD_CL_ELEM(forward_cl_elem<const volatile vint>)
+	DO_FWD_CL_ELEM(forward_cl_elem<vint>&&)
+	DO_FWD_CL_ELEM(forward_cl_elem<const vint>&&)
+	DO_FWD_CL_ELEM(forward_cl_elem<vint>&)
+	DO_FWD_CL_ELEM(forward_cl_elem<const vint>&)
 
 cout << "----------------------------------------------------------  DECL\n";
 #define DO_DECL(type)	 { typedef decltype((std::declval<type>()))  T;  for_T(LVV_STR(type));  }
@@ -49,6 +81,17 @@ cout << "----------------------------------------------------------  DECL\n";
 	DO_DECL(const int&&)
 	DO_DECL(int&)
 	DO_DECL(const int&)
+
+cout << "----------------------------------------------------------  *(*T)x \n";
+#define DO_PTR(type)	 { typedef decltype(*(type*)0)  T;  for_T(LVV_STR(type));  }
+
+	DO_PTR(const int)
+	DO_PTR(volatile int)
+	DO_PTR(const volatile int)
+	//DO_PTR(int&&)
+	//DO_PTR(const int&&)
+	//DO_PTR(int&)
+	//DO_PTR(const int&)
 
 cout << "----------------------------------------------------------  MOVE\n";
 #define DO_MOVE(type)	 { typedef decltype(std::move(std::declval<type>()))  T;  for_T(LVV_STR(type));  }
@@ -71,5 +114,4 @@ cout << "----------------------------------------------------------  FORWARD\n";
 	DO_FWD(const int&&)
 	DO_FWD(int&)
 	DO_FWD(const int&)
-
 }
