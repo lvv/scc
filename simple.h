c5d1fad6198664b31d480b23a954f14228b9ea6e
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-08-03 20:10:47 +0300

    -- added (again) cstdlib

--- simple.h
+++ simple.h
@@ -8,7 +8,7 @@
 
 //////////////////////////////////////////////////////////////////// C
 
-//#include <cstdlib>
+#include <cstdlib>
 //#include <cstddef>
 //#include <cstring>
 extern "C"  void *memcpy(void *dest, const void *src, size_t n);
