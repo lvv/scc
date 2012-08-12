d6ec5b156117650e0f86fa27973dc73721855ba1
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-08-12 12:48:30 +0300

    -- renamed range_t --> iterator_range

--- range.h
+++ range.h
@@ -18,7 +18,7 @@
 	struct range_tag{};
 
 	template<typename I>
-struct  range_t {
+struct  iterator_range {
 		typedef		      I							iterator;
 		typedef		const I							const_iterator;
 		typedef		typename std::iterator_traits<I>::value_type		value_type;
@@ -28,19 +28,19 @@ struct  range_t {
 	iterator b_, e_;
 
 	// CTOR
-	range_t()  : b_(0), e_(0) {};
-	range_t(iterator b, iterator e)  : b_(b), e_(e) {};
+	iterator_range()  : b_(0), e_(0) {};
+	iterator_range(iterator b, iterator e)  : b_(b), e_(e) {};
 
 
 	//template<typename Ct>
-	//range_t( typename std::enable_if<std::is_same<I, typename Ct::iterator>::value, Ct>::type&  C)  : b_(begin(C)), e_(end(C)) {};
+	//iterator_range( typename std::enable_if<std::is_same<I, typename Ct::iterator>::value, Ct>::type&  C)  : b_(begin(C)), e_(end(C)) {};
 
 	iterator	begin()		{ return b_; };
 	iterator	end()		{ return e_; };
 	const_iterator	begin() const	{ return b_; };
 	const_iterator	end()   const	{ return e_; };
 
-	range_t& operator= (value_type v) { for(auto& el: *this) el = v;  return *this; };
+	iterator_range& operator= (value_type v) { for(auto& el: *this) el = v;  return *this; };
 
 
 	/* to bool conversion --
@@ -59,33 +59,33 @@ struct  range_t {
 
 ////////////////////////////////////////////////////////////////  OPERATOR, -- (it1, it2) ctor
 	template<typename I>
-	eIF<is_iterator<I>(), range_t<I>>
-operator, (I b, I e)  { return range_t<I>(b,e); };
+	eIF<is_iterator<I>(), iterator_range<I>>
+operator, (I b, I e)  { return iterator_range<I>(b,e); };
 
 ////////////////////////////////////////////////////////////////  RANGE() -- makes range
 
 	template<typename I>
-	eIF<is_iterator<I>(), range_t<I>>
-range(I b, I e) { return range_t<I>(b,e); };
+	eIF<is_iterator<I>(), iterator_range<I>>
+range(I b, I e) { return iterator_range<I>(b,e); };
 
 
 	template<typename Ct>
-	eIF<is_container<Ct>(), range_t<cl_iterator<Ct>>>
-range(Ct& C) { return range_t<cl_iterator<Ct>>(std::begin(C), std::end(C)); };
+	eIF<is_container<Ct>(), iterator_range<cl_iterator<Ct>>>
+range(Ct& C) { return iterator_range<cl_iterator<Ct>>(std::begin(C), std::end(C)); };
 
-template<typename T   ,  size_t N> range_t<T   *> range(T    (&C)[N])  { return range_t<T   *>(std::begin(C), std::end(C)); };
-template<                size_t N> range_t<char*> range(char (&C)[N])  { return range_t<char*>(std::begin(C), endz    (C)); };
-template<                size_t N> range_t<const char*> range(const char (&C)[N])  { return range_t<const char*>(std::begin(C), endz    (C)); };
+template<typename T   ,  size_t N> iterator_range<T   *> range(T    (&C)[N])  { return iterator_range<T   *>(std::begin(C), std::end(C)); };
+template<                size_t N> iterator_range<char*> range(char (&C)[N])  { return iterator_range<char*>(std::begin(C), endz    (C)); };
+template<                size_t N> iterator_range<const char*> range(const char (&C)[N])  { return iterator_range<const char*>(std::begin(C), endz    (C)); };
 
 /*** we can either a pointer of an array
-	range_t<const char*>
-range(const char* p) { auto pp=p; while (*p) ++p; return range_t<const char*>(pp,p); };
+	iterator_range<const char*>
+range(const char* p) { auto pp=p; while (*p) ++p; return iterator_range<const char*>(pp,p); };
 
-	range_t<char*>
-range(char* p) { auto pp=p; while (*p) ++p; return range_t<char*>(pp,p); };
+	iterator_range<char*>
+range(char* p) { auto pp=p; while (*p) ++p; return iterator_range<char*>(pp,p); };
 */
 
-template<typename I>	struct  is_container_t <range_t<I>>	: std::true_type { };
+template<typename I>	struct  is_container_t <iterator_range<I>>	: std::true_type { };
 
 static  __attribute__((unused)) struct range_converter_t {} rng;
 
@@ -94,19 +94,19 @@ static  __attribute__((unused)) struct range_converter_t {} rng;
 	template<typename Ct>
 	eIF<
 		is_container<Ct>(),
-		range_t<typename container_iterator<Ct>::type>
+		iterator_range<typename container_iterator<Ct>::type>
 	>
 operator | (Ct& C, range_converter_t r) { return range(std::begin(C), std::end(C)); };
 
 	template<typename T, size_t N>
-	range_t<T*>
+	iterator_range<T*>
 operator | (T (&C)[N], range_converter_t r) { return range(std::begin(C), std::end(C)); };
 
 
 	template<typename Ct>
 	eIF<
 		is_container<Ct>(),
-		range_t<typename container_iterator<Ct>::type>
+		iterator_range<typename container_iterator<Ct>::type>
 	>
 operator | (range_converter_t rng, Ct& C) { return range(std::begin(C), std::end(C)); };
 
@@ -117,13 +117,13 @@ static  __attribute__((unused)) struct iot_t {} iot;
 	template<typename Ct>
 	typename std::enable_if<
 		is_container<Ct>(),
-		range_t<typename container_iterator<Ct>::type>
+		iterator_range<typename container_iterator<Ct>::type>
 	>::type
 operator | (Ct& C, iot_t r) { return range(std::begin(C), std::end(C)); };
 */
 
 template<typename T>	struct  is_range		: std::false_type {};
-template<typename T>	struct  is_range<range_t<T>>	: std::true_type  {};
+template<typename T>	struct  is_range<iterator_range<T>>	: std::true_type  {};
 
 ////////////////////////////////////////////////////////////////  RANGE OPS
 
@@ -131,10 +131,10 @@ template<typename T>	struct  is_range<range_t<T>>	: std::true_type  {};
 //  Ct1 | Ct2   ---  search() --> range	   
 /*
 	template<typename Ct>
-	typename std::enable_if <is_container<Ct>(),  range_t&>::type
+	typename std::enable_if <is_container<Ct>(),  iterator_range&>::type
 operator |       (Ct& C1, const Ct& C2)    { 
 	auto it = search(C1.begin(), C1.end(), C2.begin(), C2.end());
-	return  range_t(it, advance(it, distance(C2.end(), C2.begin())));
+	return  iterator_range(it, advance(it, distance(C2.end(), C2.begin())));
 };
 
 //  Ct1 / Ct2   ---  search() --> it
