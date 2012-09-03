2eb513db60c26bd3da9d863002725a09d0fc4ff2
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-09-03 14:58:13 +0300

    chain_range: sfinae import of wrapped range methods
    scc '88 >> range(d9); d9'
    {88, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}

--- chain_range.h
+++ chain_range.h
@@ -32,6 +32,7 @@ struct  chain_range : rn_wrapper<Rn&&> {
 		typedef		cl_iterator<Rn>							iterator;
 		typedef		cl_const_iterator<Rn>						const_iterator;
 		typedef		cl_elem_type<Rn>  						value_type;
+		typedef		size_t  							size_type;
 		typedef		typename std::iterator_traits<iterator>::difference_type	difference_type ;
 		typedef		typename std::iterator_traits<iterator>::pointer		pointer ;
 		typedef		typename std::iterator_traits<iterator>::reference		reference ;
@@ -64,17 +65,23 @@ struct  chain_range : rn_wrapper<Rn&&> {
 	value_type  	back()  const	{ return  *(std::prev(sto::endz(rn))); }  
 	reference  	back()		{ return  *(std::prev(sto::endz(rn))); }  
 
-	// CT MOD
-	eIF<has_push_back<Rn>()>	push_back( const value_type& value)	{rn.push_back(value);}
-	eIF<has_push_back<Rn>()>	push_back( value_type&& value)		{rn.push_back(std::move(value));}
-	/*
-	
-	eIF<has_push_front<Rn>()>	push_front( const value_type& value)	{rn.push_front(value);}
-	eIF<has_push_front<Rn>()>	push_front( value_type&& value)		{rn.push_front(std::move(value));}
-	
-	eIF<has_1arg_insert<Rn>()>	insert( const value_type& value)	{rn.insert(value);}
-	eIF<has_1arg_insert<Rn>()>	insert( value_type&& value)		{rn.insert(std::move(value));}
-	*/
+	// INPORT CT METHODS
+	template<class U=Rn>   eIF<has_push_back<U>()>		push_back( const value_type& value)	{rn.push_back(value);}
+	template<class U=Rn>   eIF<has_push_back<U>()>		push_back( value_type&& value)		{rn.push_back(std::move(value));}
+
+	template<class U=Rn>   eIF<has_push_front<U>()>		push_front( const value_type& value)	{rn.push_front(value);}
+	template<class U=Rn>   eIF<has_push_front<U>()>		push_front( value_type&& value)		{rn.push_front(std::move(value));}
+
+	template<class U=Rn>   eIF<has_1arg_insert<U>()>	insert( const value_type& value)	{rn.insert(value);}
+	template<class U=Rn>   eIF<has_1arg_insert<U>()>	insert( value_type&& value)		{rn.insert(std::move(value));}
+
+	template<class U=Rn>   eIF<has_pop_back<U>()>		pop_back()				{rn.pop_back();}
+	template<class U=Rn>   eIF<has_pop_front<U>()>		pop_front()				{rn.pop_front();}
+	template<class U=Rn>   eIF<has_size<U>(), size_type>	size()					{return rn.size();}
+	template<class U=Rn>   eIF<has_empty<U>(), bool>	empty()					{return rn.empty();}
+	template<class U=Rn>   eIF<has_back<U>(), value_type>	back()					{return rn.back();}
+	template<class U=Rn>   eIF<has_front<U>(), value_type>	front()					{return rn.front();}
+
  };
 
 
